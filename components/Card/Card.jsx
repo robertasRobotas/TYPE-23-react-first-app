4d02ee78541b1e9cab6dad3902d0cc86a9017588
Robertas Ankudovicius <robertas.ankudovicius@twoday.com>
2024-11-29 09:13:01 +0200

    Added mapping / conditional rendering

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 const Card = ({ title, imgUrl }) => {
+  const [isTitleVisible, setTitleVivible] = useState(false);
+
   return (
     <div className={styles.main}>
       <img src={imgUrl} />
-      <h4>{title}</h4>
+      <button
+        onClick={() => {
+          setTitleVivible(!isTitleVisible);
+        }}
+      >
+        {isTitleVisible ? <>Hide</> : <>Show</>}
+      </button>
+      {isTitleVisible && <h4>{title}</h4>}
     </div>
   );
 };
--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -5,36 +5,36 @@ import styles from "./styles.module.css";
 const Main = () => {
   const [destinations, setDestinations] = useState([
     {
+      id: "aaa1",
       title: "Visit Crete",
       imgUrl:
         "https://tristanbalme.com/wp-content/uploads/2023/04/Tristan-Balme-the-best-beaches-in-crete-for-you-to-visit.jpg",
     },
     {
+      id: "aaa2",
       title: "Tenerife",
       imgUrl:
         "https://content.r9cdn.net/rimg/dimg/67/34/b7e3a5a4-city-80676-166ea9244ff.jpg?crop=true&width=1366&height=768&xhint=3868&yhint=3457",
     },
     {
+      id: "aaa3",
       title: "Egypt",
       imgUrl:
         "https://i.natgeofe.com/k/109a4e08-5ebc-48a5-99ab-3fbfc1bbd611/Giza_Egypt_KIDS_0123_16x9.jpg",
     },
+    {
+      id: "aaa4",
+      title: "Turkiye",
+      imgUrl:
+        "https://www.oliverstravels.com/blog/wp-content/uploads/2024/02/Oludeniz-1.jpg",
+    },
   ]);
-
-  const [isAllDestinationsAvailable, setAllDestinationsAvailable] =
-    useState(false);
 
   return (
     <div className={styles.main}>
-      <Card title={destinations[0].title} imgUrl={destinations[0].imgUrl} />
-      <Card title={destinations[1].title} imgUrl={destinations[1].imgUrl} />
-      <Card title={destinations[2].title} imgUrl={destinations[2].imgUrl} />
-
-      {isAllDestinationsAvailable ? (
-        <>All destinations availible</>
-      ) : (
-        <>Not all destinations available</>
-      )}
+      {destinations.map((d) => {
+        return <Card key={d.id} title={d.title} imgUrl={d.imgUrl} />;
+      })}
     </div>
   );
 };
